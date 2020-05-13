321b4def6c8e325684c991e29cd0618934e639bc
gzapodea <gzapodea@cisco.com>
2020-05-13 14:11:53 -0700

    added script to save to file

---
+++ dnac_rest_apis_file.py
@@ -0,0 +1,116 @@
+
+# -*- coding: utf-8 -*-
+"""
+
+Cisco DNA Center Command Runner
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
+__author__ = "Gabriel Zapodeanu TME, ENB"
+__email__ = "gzapodea@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2020 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+
+import os
+import sys
+import time
+import datetime
+import requests
+import urllib3
+import json
+
+from urllib3.exceptions import InsecureRequestWarning  # for insecure https warnings
+from requests.auth import HTTPBasicAuth  # for Basic Auth
+
+urllib3.disable_warnings(InsecureRequestWarning)  # disable insecure https warnings
+
+
+# Cisco DNA Center info
+
+username = 'Admin'
+password = 'Clive.06'
+DNAC_URL = 'https://10.93.141.35'
+
+
+DNAC_AUTH = HTTPBasicAuth(username, password)
+
+
+def get_dnac_jwt_token(dnac_auth):
+    """
+    Create the authorization token required to access DNA C
+    Call to Cisco DNA Center - /api/system/v1/auth/login
+    :param dnac_auth - Cisco DNA Center Basic Auth string
+    :return: Cisco DNA Center JWT token
+    """
+    url = DNAC_URL + '/dna/system/api/v1/auth/token'
+    header = {'content-type': 'application/json'}
+    response = requests.post(url, auth=dnac_auth, headers=header, verify=False)
+    dnac_jwt_token = response.json()['Token']
+    return dnac_jwt_token
+
+
+def get_all_device_info(limit, dnac_jwt_token):
+    """
+    The function will return all network devices info, using the specified limit of devices/API Call
+    :param limit: the number of devices to return per API call
+    :param dnac_jwt_token: Cisco DNA C token
+    :return: DNA C device inventory info
+    """
+    offset = 1
+    all_devices_list = []
+    all_devices_info = ['']  # assign a value, to make sure the API call will run at least once
+    while all_devices_info:
+        all_devices_info = ''
+        url = DNAC_URL + '/dna/intent/api/v1/network-device?offset=' + str(offset) + '&limit=' + str(limit)
+        header = {'content-type': 'application/json', 'x-auth-token': dnac_jwt_token}
+        all_devices_response = requests.get(url, headers=header, verify=False)
+        all_devices_json = all_devices_response.json()
+        all_devices_info = all_devices_json['response']
+        all_devices_list += all_devices_info
+        offset += limit
+    return all_devices_list
+
+
+def get_epoch_current_time():
+    """
+    This function will return the epoch time for the {timestamp}
+    :return: epoch time including msec
+    """
+    epoch = time.time()*1000
+    return int(epoch)
+
+
+def main():
+    # get the Cisco DNA Center Auth
+    dnac_auth = get_dnac_jwt_token(DNAC_AUTH)
+
+    # get all the devices info, 500 devices collect per each API call (this is the max)
+    all_devices_info = get_all_device_info(500, dnac_auth)
+
+    # save the device info in a file, one device per line. File name = 'device_inventory_file.json'
+    with open('device_inventory_file.json', 'w') as f:
+        for device in all_devices_info:
+            f.write(json.dumps(device) + '\n')
+
+
+if __name__ == '__main__':
+    main()
+
+
